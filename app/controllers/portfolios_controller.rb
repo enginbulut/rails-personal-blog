4a95fb1462862377efa99541b054dd852cd57c59
Engin Bulut <engin@deliv.co>
2020-03-15 17:00:44 +0300

    implemented authorization portfolio and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,9 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: %i[edit update show destroy]
   layout 'portfolio'
+  access all: %i[show index angular],
+         user: { except: %i[destroy new create update edit] },
+         site_admin: :all
 
   def index
     @portfolio_items = Portfolio.all
