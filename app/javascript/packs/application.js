479ec88ca1cc05c7240bc2a6db61d6e7c5a4d06f
Engin Bulut <engin@deliv.co>
2020-03-29 19:16:47 +0300

    implemented persistent position updates for drag and drop interface

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,5 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('packs/portfolio')
 
 
--- app/javascript/packs/portfolio.js
+++ app/javascript/packs/portfolio.js
@@ -1,3 +1,29 @@
+function set_positions() {
+  $(".card").each(function(i) {
+    $(this).attr("data-pos", i + 1 );
+  })
+}
+
 $( document ).ready(function() {
+  set_positions();
   $(".sortable").sortable();
+  $(".sortable").sortable().bind("sortupdate", function(e, i) {
+    updated_order = []
+    set_positions();
+    $(".card").each(function(i) {
+      updated_order.push({
+        id: $(this).data("id"),
+        position: i + 1
+      })
+    });
+
+    $.ajax({
+      type: "PUT",
+      url: "/portfolios/sort",
+      data: {
+        order: updated_order
+      }
+    });
+
+  });
 });
